63930ccfed4c88479a35afce7d90742c0ae39862
armandupe <symbolic28@gmail.com>
2020-12-10 11:02:32 +0300

    somefixes

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ new Vue({
     el: '#app',
     data: {
         inputsFirstRow: [
-            {key: '1'},
-            {key: '2'},
-            {key: '3'},
-            {key: '+'}
+            { key: '1' },
+            { key: '2' },
+            { key: '3' },
+            { key: '+' }
         ],
         inputsSecondRow: [
-            {key: '4'},
-            {key: '5'},
-            {key: '6'},
-            {key: '-'}
+            { key: '4' },
+            { key: '5' },
+            { key: '6' },
+            { key: '-' }
         ],
         inputsThirdRow: [
-            {key: '7'},
-            {key: '8'},
-            {key: '9'},
-            {key: '*'}
+            { key: '7' },
+            { key: '8' },
+            { key: '9' },
+            { key: '*' }
         ],
         inputsFourthRow: [
-            {key: '/'},
-            {key: '0'},
-            {key: '.'}
+            { key: '/' },
+            { key: '0' },
+            { key: '.' }
         ],
     },
     methods: {
@@ -42,22 +42,19 @@ new Vue({
             const darkMode = document.getElementById("dark-mode");
             const theme = document.getElementById("theme");
             if (theme.getAttribute("href") == "light.css") {
-              theme.href = "dark.css";
-              darkMode.innerHTML = "Light Mode ☀";
+                theme.href = "dark.css";
+                darkMode.innerHTML = "Light Mode ☀";
             } else {
-              theme.href = "light.css";
-              darkMode.innerHTML = "Dark Mode 🌙";
+                theme.href = "light.css";
+                darkMode.innerHTML = "Dark Mode 🌙";
             }
         },
         stopInput() {
             const result = document.querySelector('.result');
-            if (result.value == '+' ||
-                result.value == '-' ||
-                result.value == '*' ||
-                result.value == '/' ||
-                result.value == '.') {
+            const regExp = /([+]{2}|[/+]{2}|[/-]{2}|[/*]{2}|[-*]{2}|[*+]{2}|[-.]{2}|[+.]{2}|[*.]{2}|[/.]{2}|[+-]{2})/g;
+            if (result.value.match(regExp) || result.value.includes(undefined)) {
                 result.value = '';
-            }   
+            }
         },
     },
 })
